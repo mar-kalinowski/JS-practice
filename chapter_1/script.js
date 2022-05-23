e0e345fb1f9a307a35ac9a29691527767830854a
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-05-23 18:43:47 +0200

    script.js modified

--- chapter_1/script.js
+++ chapter_1/script.js
@@ -1,10 +1,36 @@
-let age = 25
-let year = 2022
+let age = 25;
+let year = 2022;
+let email = "jan@nowak.pl";
 
-age = 30
+age = 30;
 
-console.log(age, year)
+console.log(age, year);
 
-const points = 100
+const points = 100;
 // points = 50
-console.log(points)
+console.log(points);
+
+let firstName = "Jan";
+let lastName = "Nowak";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+console.log(fullName[0]);
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf("@");
+console.log(index);
+
+let email_2 = "john@smith.co.uk";
+
+let result_2 = email_2.lastIndexOf("h");
+console.log(result_2);
+
+let result_3 = email_2.slice(2, 5);
+console.log(result_3);
+
+let resSubstr = email.substr(2,5);
