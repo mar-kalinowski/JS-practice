d0c895eeb67f09adf19580650963d92d1095fbe3
Marcin Kalinowski <mjk.gdansk@gmail.com>
2022-05-25 13:07:40 +0200

    chapter_4 added

---
+++ chapter_4/script.js
@@ -0,0 +1,24 @@
+// function declaration
+
+// function greet() {
+//   console.log("hello there");
+// }
+
+// function expression
+
+// const speak = (name = 'jan', time = 'night') => {
+//   console.log(`good ${time} ${name}!`);
+// };
+
+// greet();
+
+// speak();
+// speak('john', 'night');
+
+const calcArea = (radius) => {
+let area = 3.14 *radius**2;
+return area
+}
+
+const area = calcArea(5)
+console.log(area)
